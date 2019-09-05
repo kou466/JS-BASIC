985d117dea65b352be1dfc9d84291c18b992c149
kou466 <32214586+kou466@users.noreply.github.com>
2019-09-05 22:40:21 +0900

    Basic learning

---
+++ base/index.js
@@ -0,0 +1,12 @@
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() {
+  window.addEventListener("click", handleClick);
+}
+init();
